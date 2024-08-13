84dfcc78e5df67eef79894603b51ad420087da26
alexcy <bondaralexcy@gmail.com>
2024-08-13 21:57:43 +0300

    Add generics and permissions

--- habits/urls.py
+++ habits/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
-from rest_framework.routers import SimpleRouter
-
 from habits.apps import HabitsConfig
-from habits.views import HabitViewSet, PublicHabitListAPIView
+from habits.views import HabitsListAPIView, HabitsRetrieveAPIView, \
+    HabitsCreateAPIView, HabitsUpdateAPIView, \
+    HabitsDestroyAPIView, PublicHabitListAPIView
 
 app_name = HabitsConfig.name
 
-router = SimpleRouter()
-router.register("", HabitViewSet, basename="habits")
-
 urlpatterns = [
-    path("public/", PublicHabitListAPIView.as_view(), name="public"),
-] + router.urls
+    path("list/", HabitsListAPIView.as_view(), name="habits_list"),
+    path("<int:pk>/", HabitsRetrieveAPIView.as_view(), name="habits_retrieve"),
+    path("create/", HabitsCreateAPIView.as_view(), name="habits_create"),
+    path("<int:pk>/update/", HabitsUpdateAPIView.as_view(), name="habits_update"),
+    path("<int:pk>/delete/", HabitsDestroyAPIView.as_view(), name="habits_delete"),
+    path("public/", PublicHabitListAPIView.as_view(), name="public_list"),
+]
--- habits/views.py
+++ habits/views.py
@@ -1,10 +1,5 @@
-from django.shortcuts import render
-from django.utils.decorators import method_decorator
-from drf_yasg.utils import swagger_auto_schema
-# Create your views here.
 from rest_framework import generics, viewsets
-from rest_framework.filters import OrderingFilter, SearchFilter
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from habits.models import Habit
 from habits.paginations import HabitPaginator
@@ -12,27 +7,69 @@ from habits.permissions import IsOwner
 from habits.serializers import HabitSerializer
 
 
-@method_decorator(
-    name="list", decorator=swagger_auto_schema(operation_description="Список привычек")
-)
-class HabitViewSet(viewsets.ModelViewSet):
-    serializer_class = HabitSerializer
-    permission_classes = [IsAuthenticated, IsOwner]
-    pagination_class = HabitPaginator
-    filter_backends = [SearchFilter, OrderingFilter]
-    search_fields = ("action",)
-    ordering_fields = ("time",)
 
-    def get_queryset(self):
-        return Habit.objects.filter(user=self.request.user.pk).order_by("id")
+class HabitsCreateAPIView(generics.CreateAPIView):
+    """ Создание привычки
+    """
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = (IsAuthenticated,)
 
     def perform_create(self, serializer):
+        """ Добавляем текущего пользователя
+        в качест ве владельца привычки"""
         new_habit = serializer.save()
         new_habit.user = self.request.user
         new_habit.save()
 
 
+class HabitsListAPIView(generics.ListAPIView):
+    """ Отображение привычек пользователя
+    """
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = (IsAuthenticated,)
+    pagination_class = HabitPaginator
+
+    def get_queryset(self):
+        queryset = self.queryset.filter(user=self.request.user)
+        return queryset
+
+
+class HabitsRetrieveAPIView(generics.RetrieveAPIView):
+    """ Просмотр выбранной привычки
+    """
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = (IsAuthenticated, IsOwner)
+
+
+class HabitsUpdateAPIView(generics.UpdateAPIView):
+    """ Обновление выбранной привычки
+    """
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = (IsAuthenticated, IsOwner)
+
+    def perform_update(self, serializer):
+        habit = serializer.save()
+        habit.save()
+
+
+class HabitsDestroyAPIView(generics.DestroyAPIView):
+    """ Удаление выбранной привычки
+     """
+
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = (IsAuthenticated, IsOwner)
+
+
+
 class PublicHabitListAPIView(generics.ListAPIView):
+    """ Список публичных привычек
+    """
     serializer_class = HabitSerializer
     queryset = Habit.objects.filter(is_public=True)
     pagination_class = HabitPaginator
+    permission_classes = (AllowAny,)
